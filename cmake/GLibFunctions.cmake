36149bfed397f4b6e266d6e5c339591a7e1da828
dawkagaming <147207983+dawkagaming@users.noreply.github.com>
2026-08-30 13:16:10 +0200

    Align resources compilation to meson implementation

--- cmake/GLibFunctions.cmake
+++ cmake/GLibFunctions.cmake
@@ -5,33 +5,17 @@ find_program(GLIB_COMPILE_SCHEMAS_BINARY glib-compile-schemas REQUIRED)
 
 function(glib_compile_resources)
     set(flags
-        GENERATE_HEADER
-        MANUAL_REGISTER
-        INTERNAL
-        EXTERNAL_DATA)
+        GRESOURCE_BUNDLE)
     set(args
-        MIN_VERSION
-        CNAME
-        SOURCEDIR
-        TARGET
+        C_NAME
         FILE
         OUTPUT_VAR)
-    set(listArgs)
+    set(listArgs
+        EXTRA_ARGS
+        DEPENDENCIES
+        SOURCE_DIR)
 
     cmake_parse_arguments(arg "${flags}" "${args}" "${listArgs}" ${ARGN})
-
-    # Check version
-    if(arg_MIN_VERSION)
-        execute_process(COMMAND "${GLIB_COMPILE_RESOURCES}" "--version" OUTPUT_VARIABLE GLIB_COMPILE_RESOURCES_VERSION OUTPUT_STRIP_TRAILING_WHITESPACE RESULT_VARIABLE EXIT_CODE)
-
-        if(NOT EXIT_CODE EQUAL 0)
-            message(FATAL_ERROR "[glib_compile_resources]: glib-compile-resources failed with exit code ${EXIT_CODE} when checking version")
-        endif()
-        
-        if("${GLIB_COMPILE_RESOURCES_VERSION}" VERSION_LESS "${arg_MIN_VERSION}")
-            message(FATAL_ERROR "[glib_compile_resources]: Minimum version requested is ${arg_MIN_VERSION}, but found version ${GLIB_COMPILE_RESOURCES_VERSION}, which is lower!")
-        endif()
-    endif()
 
     if(NOT IS_ABSOLUTE "${arg_FILE}")
         set(arg_FILE "${CMAKE_CURRENT_SOURCE_DIR}/${arg_FILE}")
@@ -47,80 +31,86 @@ function(glib_compile_resources)
         message(FATAL_ERROR "[glib_compile_resources]: OUTPUT_VAR is required!")
     endif()
 
-    # Parse other options
+    # Assemble command options
     set(COMMON_ARGS)
 
-    if(arg_MANUAL_REGISTER)
-        list(APPEND COMMON_ARGS "--manual-register")
+    list(APPEND COMMON_ARGS ${arg_EXTRA_ARGS})
+
+    if(arg_C_NAME)
+        list(APPEND COMMON_ARGS "--c-name" "${arg_C_NAME}")
     endif()
 
-    if(arg_INTERNAL)
-        list(APPEND COMMON_ARGS "--internal")
-    endif()
+    if(arg_SOURCE_DIR)
+        foreach(DIR_TO_CHECK "${arg_SOURCE_DIR}")
+            if(NOT IS_ABSOLUTE "${DIR_TO_CHECK}")
+                set(DIR_TO_CHECK "${CMAKE_CURRENT_SOURCE_DIR}/${DIR_TO_CHECK}")
+            endif()
 
-    if(arg_EXTERNAL_DATA)
-        list(APPEND COMMON_ARGS "--external-data")
-    endif()
-
-    if(arg_CNAME)
-        list(APPEND COMMON_ARGS "--c-name" "${arg_CNAME}")
-    endif()
-
-    if(NOT arg_SOURCEDIR)
-        get_filename_component(arg_SOURCEDIR "${arg_FILE}" DIRECTORY)
-    endif()
-
-    if(IS_DIRECTORY "${arg_SOURCEDIR}")
-        list(APPEND COMMON_ARGS "--sourcedir" "${arg_SOURCEDIR}")
+            if(IS_DIRECTORY "${DIR_TO_CHECK}")
+                list(APPEND COMMON_ARGS "--sourcedir" "${DIR_TO_CHECK}")
+            else()
+                message(FATAL_ERROR "[glib_compile_resources]: Path passed to SOURCE_DIR (${DIR_TO_CHECK}) is not a proper directory path!")
+            endif()
+        endforeach()
     else()
-        message(FATAL_ERROR "[glib_compile_resources]: Path passed as SOURCEDIR (${arg_SOURCEDIR}) is not a proper path!")
+        get_filename_component(arg_SOURCE_DIR "${arg_FILE}" DIRECTORY)
+
+        list(APPEND COMMON_ARGS "--sourcedir" "${arg_SOURCE_DIR}")
     endif()
 
-    if(NOT arg_TARGET)
-        set(arg_TARGET "${CMAKE_CURRENT_BINARY_DIR}/gresources.c")
+    # Get dependent files list
+    set(DEPENDENT_FILES)
 
-        message(STATUS "TARGET not set, defaulting to ${arg_TARGET}")
+    list(APPEND DEPENDENT_FILES ${arg_DEPENDENCIES} "${arg_FILE}")
+
+    execute_process(COMMAND "${GLIB_COMPILE_RESOURCES_BINARY}" ${COMMON_ARGS} "--generate-dependencies" "${arg_FILE}" OUTPUT_VARIABLE GENERATED_DEPENDENCIES OUTPUT_STRIP_TRAILING_WHITESPACE RESULT_VARIABLE EXIT_CODE)
+
+    if(NOT EXIT_CODE EQUAL 0)
+        message(FATAL_ERROR "[glib_compile_resources]: glib-compile-resources failed with exit code ${EXIT_CODE} when getting dependent files!")
     endif()
 
-    get_filename_component(FILE_EXTENSION "${arg_TARGET}" LAST_EXT)
+    string(REGEX REPLACE "\r\n|\n" ";" GENERATED_DEPENDENCIES "${GENERATED_DEPENDENCIES}")
 
-    if(NOT "${FILE_EXTENSION}" STREQUAL ".c")
-        string(APPEND arg_TARGET ".c")
-    endif()
+    list(APPEND DEPENDENT_FILES ${GENERATED_DEPENDENCIES})
 
-    list(APPEND SOURCE_ARGS ${COMMON_ARGS} "--target" "${arg_TARGET}" "--generate-source")
+    # Get input file directory and its name without extensions
+    get_filename_component(FILE_NAME "${arg_FILE}" NAME_WE)
 
-    # Execute the command
-    add_custom_command(
-        OUTPUT "${arg_TARGET}"
-        COMMAND "${GLIB_COMPILE_RESOURCES_BINARY}" ${SOURCE_ARGS} "${arg_FILE}"
-        DEPENDS "${arg_FILE}"
-        CODEGEN
-        COMMENT "[glib_compile_resources]: Compiling resources from file ${arg_FILE} into source file: ${arg_TARGET}"
-    )
+    # Final execution
+    if(arg_GRESOURCE_BUNDLE)
+        set(TARGET_GRESOURCE "${CMAKE_CURRENT_BINARY_DIR}/${FILE_NAME}.gresource")
 
-    if(arg_GENERATE_HEADER)
-        get_filename_component(TARGET_DIR "${arg_TARGET}" DIRECTORY)
-
-        get_filename_component(TARGET_HEADER "${arg_TARGET}" NAME_WLE)
-        
-        set(TARGET_HEADER "${TARGET_DIR}/${TARGET_HEADER}.h")
-
-        list(APPEND HEADER_ARGS ${COMMON_ARGS} "--target" "${TARGET_HEADER}" "--generate-header")
+        list(APPEND GRESOURCE_ARGS ${COMMON_ARGS} "--target" "${TARGET_GRESOURCE}")
 
         add_custom_command(
-            OUTPUT "${TARGET_HEADER}"
-            COMMAND "${GLIB_COMPILE_RESOURCES_BINARY}" ${HEADER_ARGS} "${arg_FILE}"
-            DEPENDS "${arg_FILE}"
+            OUTPUT "${TARGET_GRESOURCE}"
+            COMMAND "${GLIB_COMPILE_RESOURCES_BINARY}" ${GRESOURCE_ARGS} "${arg_FILE}"
+            DEPENDS ${DEPENDENT_FILES}
             CODEGEN
-            COMMENT "[glib_compile_resources]: Compiling resources from file ${arg_FILE} into header file: ${TARGET_HEADER}"
+            COMMENT "[glib_compile_resources]: Compiling resources from file ${arg_FILE}"
         )
+
+        list(APPEND OUTPUT_TARGETS "${TARGET_GRESOURCE}")
+        set("${arg_OUTPUT_VAR}" "${OUTPUT_TARGETS}" PARENT_SCOPE)
+    else()
+        set(TARGET_SOURCE "${CMAKE_CURRENT_BINARY_DIR}/${FILE_NAME}.c")
+        list(APPEND SOURCE_ARGS ${COMMON_ARGS} "--generate-source" "--target" "${TARGET_SOURCE}")
+
+        set(TARGET_HEADER "${CMAKE_CURRENT_BINARY_DIR}/${FILE_NAME}.h")
+        list(APPEND HEADER_ARGS ${COMMON_ARGS} "--generate-header" "--target" "${TARGET_HEADER}")
+
+        add_custom_command(
+            OUTPUT "${TARGET_SOURCE}" "${TARGET_HEADER}"
+            COMMAND "${GLIB_COMPILE_RESOURCES_BINARY}" ${SOURCE_ARGS} "${arg_FILE}"
+            COMMAND "${GLIB_COMPILE_RESOURCES_BINARY}" ${HEADER_ARGS} "${arg_FILE}"
+            DEPENDS ${DEPENDENT_FILES}
+            CODEGEN
+            COMMENT "[glib_compile_resources]: Compiling resources from file ${arg_FILE}"
+        )
+
+        list(APPEND OUTPUT_TARGETS "${TARGET_SOURCE}" "${TARGET_HEADER}")
+        set("${arg_OUTPUT_VAR}" "${OUTPUT_TARGETS}" PARENT_SCOPE)
     endif()
-
-
-    list(APPEND OUTPUT_FILES "${arg_TARGET}" "${TARGET_HEADER}")
-
-    set("${arg_OUTPUT_VAR}" "${OUTPUT_FILES}" PARENT_SCOPE)
 endfunction()
 
 function(glib_compile_schemas)
